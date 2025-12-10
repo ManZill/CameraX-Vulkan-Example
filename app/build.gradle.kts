afab1789cd8ec3184cc42f1d70093ff1138644d7
ManZill <matvei.zkuk@gmail.com>
2025-12-10 21:26:37 +0400

    Initialize CameraX-Vulkan-Example Android project
    - Establish initial project structure using Gradle Kotlin DSL and Version Catalog (`libs.versions.toml`).
    - Configure `app` module targeting SDK 36 with `minSdk` 24.
    - Integrate Jetpack Compose, Material 3, and AndroidX lifecycle dependencies.
    - Add entry point `MainActivity` and placeholder `MainScreen` composable.
    - Include standard application resources, Manifest, and ProGuard rules.
    - Add Gradle wrapper scripts (v8.13) and IDE configuration files.

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.manzill.example.camxvk"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.manzill.example.camxvk"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.fromTarget(JavaVersion.VERSION_21.toString())
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.compose.foundation)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.androidx.compose.material3.adaptive)
+
+    implementation(libs.androidx.activity.compose)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.androidx.compose.runtime.rxjava2)
+}
---
+++ app/src/main/kotlin/com/manzill/example/camxvk/MainScreen.kt
@@ -0,0 +1,18 @@
+package com.manzill.example.camxvk
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    MainScreen()
+}
